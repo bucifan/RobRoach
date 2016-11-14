66e31100b308f36f3d730240b242c391e42ec8b1
Rob Roach <roberoach@hotmail.com>
2016-11-14 18:07:42 +0000

    start

--- js/smbb.js
+++ js/smbb.js
@@ -1,18 +1,26 @@
-setTimeout(rotateM, 500);
+rotateM();
 
 function rotateM(){
     var logo = $("#smlogo");
-    TweenLite.set(".cardWrapper", {perspective:800});
+    TweenLite.set(".cardWrapper", {perspective:400});
     TweenLite.set(".loadingcard", {transformStyle:"preserve-3d"});
     TweenLite.set(".back", {rotationY:-180});
     TweenLite.set([".back", ".front"], {backfaceVisibility:"hidden"});
+    var rty = 180;
+    TweenLite.to($(".loadingcard"), 2, {rotationY:180});
+    setInterval(function(){
+        rty=rty+180;
+        TweenLite.to($(".loadingcard"), 2, {rotationY:rty});
+      
+    },2100 );
+    //setTimeout(function(){ setInterval(function(){TweenLite.to($(".loadingcard"), 2, {rotationY:180});},2100 )},2100);
 
     $(".cardWrapper").hover(
       function() {
-        TweenLite.to($(this).find(".card"), 1.2, {rotationY:180});
+        TweenLite.to($(this).find(".loadingcard"), 1.2, {rotationY:180});
       },
       function() {
-        TweenLite.to($(this).find(".card"), 1.2, {rotationY:0});  
+        TweenLite.to($(this).find(".loadingcard"), 1.2, {rotationY:360});  
       }
     );
 }
