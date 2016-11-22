407ad88b650732bc249672df09d46c80df9a4e9e
Rob Roach <roberoach@hotmail.com>
2016-11-22 13:20:09 +0000

    hdr

--- js/osubb.js
+++ js/osubb.js
@@ -4,7 +4,11 @@ $.getJSON("https://bucifan-api.azurewebsites.net/osubb")
       for(var i=0;i<osubb.games.length;i++){
           $(".schedule2016").append("<div id='gameitem"+i+"' class='gameitem' data-opinit='"+osubb.games[i].OppNH+"' );'></div><div id='gamedtl"+i+"' class='hidgamedtls' onclick='toggledtls("+i+");'></div> ");
           $("#gameitem"+i).append("<span class='schdate'> "+osubb.games[i].date+"</span>");
-          $("#gameitem"+i).append("<span class='schteam'> <img src='/img/bball/"+osubb.games[i].OppImage+"' class='teamimg'/>" +osubb.games[i].Opp+"</span>");
+          var awayTxt="";
+          if(osubb.games[i].Loc == "Away"){
+            awayTxt="@"; 
+          }
+          $("#gameitem"+i).append("<span class='schteam'> <img src='/img/bball/"+osubb.games[i].OppImage+"' class='teamimg'/> <span class='homeaway'>"+awayTxt+" </span>" +osubb.games[i].Opp+"</span>");
           if((osubb.games[i].OSUScore==0)&&(osubb.games[i].OppScore==0)){
               $("#gameitem"+i).append("<span class='schtime' id='gamespan"+i+"'  data-at='0' >"+osubb.games[i].Time+"</span>");
               switch(osubb.games[i].TV){
