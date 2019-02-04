2bd6e6099fcb599c82c18ce2f769ee8efef9ecb2
Rob Roach <roberoach@hotmail.com>
2019-02-04 19:40:55 +0000

    add icon

--- js/fb.js
+++ js/fb.js
@@ -11,14 +11,14 @@ function gohome(){
 function prevSYear(){
   SYear--;
   $(".yrseldtls").html("<span class='loadingData'>Loading schedule for year: " + SYear+ "</span>");
-  $(".currYearnav").html(SYear);
+  $("#currYearnav").html(SYear);
   getGames();
   
 }
 function nextSYear(){
     SYear++;
     $(".yrseldtls").html("<span class='loadingData'>Loading schedule for year: " + SYear+ "</span>");
-    $(".currYearnav").html(SYear);
+    $("#currYearnav").html(SYear);
     getGames();
 }
 
@@ -36,6 +36,7 @@ function loadScheduleItems(){
     var game11 = $("#gameitem10");
     var game12 = $("#gameitem11");
     var game13 = $("#gameitem12");
+
     TweenLite.to(game1,1,{opacity:1.0});
     TweenLite.to(game2,1,{opacity:1.0,  delay:.5});
     TweenLite.to(game3,1,{opacity:1.0,  delay:1});
@@ -49,6 +50,14 @@ function loadScheduleItems(){
     TweenLite.to(game11,1,{opacity:1.0,  delay:5});
     TweenLite.to(game12,1,{opacity:1.0,  delay:5.5});
     TweenLite.to(game13,1,{opacity:1.0,  delay:5.5});
+    if( $("#gameitem13").length>0){
+         var game14 = $("#gameitem13");
+         TweenLite.to(game14,1,{opacity:1.0,  delay:6.0});
+    }
+    if( $("#gameitem14").length>0){
+         var game15 = $("#gameitem14");
+         TweenLite.to(game15,1,{opacity:1.0,  delay:6.0});
+    }
     $(".loadingData").slideUp(500,function(){$(".loadingData").remove();})
 }
 function getGames(){
@@ -56,23 +65,23 @@ $.getJSON("https://bucifan-api.azurewebsites.net/fbs/"+SYear)
   .done(function(season){ 
       for(var i=0;i<season.games.length;i++){
           $(".yrseldtls").append("<div id='gameitem"+i+"' class='gameitem' data-opinit='"+season.games[i].Opp+"' onclick='toggledtls("+i+");'></div><div id='gamedtl"+i+"' class='hidgamedtls' onclick='toggledtls("+i+");'></div> ");
-          $("#gameitem"+i).append("<div class='gameitemleft'><span class='schdate'> "+season.games[i].GameDate+"</span></div><div class='gameitemmid'><img src='/img/logos/"+season.games[i].OppImage+"' /></div>");
+          $("#gameitem"+i).append("<div class='gameitemleft'><span class='schdate' > "+season.games[i].GameDate+"</span></div><div class='gameitemmid' id='gameid"+i+"'><img src='/img/logos/"+season.games[i].OppImage+"' /></div>");
           $("#gameitem"+i).append("<div class='gameitemright'><span class='schteam'>"+season.games[i].Opp+"</span></div>");
           //if(i==11){
         //      for(var j=0;j<4;j++){ 
         //        $("#gamedtl"+i).append("<img src='/img/screwblueBrutusStompTSUN.gif'/>");
         //      }
         //  }
-        //  if((games.y2017[i].OSUScore==0)&&(games.y2017[i].OppScore==0)){
+        if((season.games[i].OSUScore!=0)&&(season.games[i].OppScore!=0)){
         //      $("#gameitem"+i).append("<span class='schtime'  data-at='0' >"+games.y2017[i].Start+"</span>");
         //  } else {
-        //     var worl = "";
-         //    if(games.y2017[i].OSUScore>games.y2017[i].OppScore){
-        //        worl = "<b style='color:green'> W </b>";
-         //    } else {
-         //       worl = "<b style='color:red'> L </b>"; 
-         //    }
-        //     $("#gameitem"+i).append("<span class='schtime' style='color:black' >"+worl+" OSU: " + games.y2017[i].OSUScore + " " + games.y2017[i].OppShort + ": "+games.y2017[i].OppScore+"</span>"); 
+           var worl = "";
+           if(season.games[i].OSUScore>season.games[i].OppScore){
+                worl = "<b style='color:green'> W </b>";
+            } else {
+               worl = "<b style='color:red'> L </b>"; 
+            }
+             $("#gameid"+i).append("<span class='schtime' style='color:black' >"+worl+" OSU: " + season.games[i].OSUScore + " " + season.games[i].OppShort + ": "+season.games[i].OppScore+"</span>"); 
         //     $("#gamedtl"+i).append("<div style='float:left'><b> Total Yards: </b> " + games.y2017[i].tyards + "<br/><b> Passing Yards: </b> " + games.y2017[i].pyards + "<br/><b> Rushing Yards: </b> " + games.y2017[i].ryards + "<br/></div>");
         //     $("#gamedtl"+i).append("<div style='float:right'><b> Average Yards per play: </b> " + games.y2017[i].ypp + "<br/><b> Yards given up: </b> " + games.y2017[i].ygivenup + "<br/><br/></div>");
         //     $("#gamedtl"+i).append("<br/><div style='width:100%;text-align:left;display:block'><b> Notes: </b> " + games.y2017[i].mynotes + "<br/></div>");
